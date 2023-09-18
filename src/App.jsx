ea2b9baf7102d42db75d9b645be73d5819dced05
Santiago Trini <santiagotrini@gmail.com>
2023-09-18 14:12:15 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  return <h1>Hola, mi app</h1>
+};
+
+export default App
---
+++ src/Contacto.jsx
@@ -0,0 +1,5 @@
+const Contacto = props => {
+  return <h1>Hola, soy la pagina de contacto</h1>;
+};
+
+export default Contacto;
